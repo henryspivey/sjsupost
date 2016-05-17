e4da4d6b3b29a4e01b1e5ca32bffcf5cf10ab8b0
Henry Spivey <spivotron@gmail.com>
2016-05-16 18:01:04 -0700

    Update settings.py

--- sjsupost/settings.py
+++ sjsupost/settings.py
@@ -128,7 +128,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'enrico567@gmail.com'
-EMAIL_HOST_PASSWORD = '5pivotron'
+EMAIL_HOST_PASSWORD = '************'
 EMAIL_PORT = 587
 
 
